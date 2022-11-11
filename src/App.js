2f303cfeccce871946e50ea3c349f50425d6eb2c
kateFrontend <ecdanilova@gmail.com>
2022-11-11 11:19:13 -0500

    setup project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import './App.css';
-
+import React, { useState, useEffect } from 'react'
+import {
+  FaEnvelopeOpen,
+  FaUser,
+  FaCalendarTimes,
+  FaMap,
+  FaPhone,
+  FaLock,
+} from 'react-icons/fa'
+const url = 'https://randomuser.me/api/'
+const defaultImage = 'https://randomuser.me/api/portraits/men/75.jpg'
 function App() {
-  return (
-    <div>
-     Hello
-    </div>
-  );
+  return <h2>random user starter</h2>
 }
 
-export default App;
+export default App
