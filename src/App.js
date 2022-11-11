fd11a79f583fb52e6bf24496d27c24858054a13c
kateFrontend <ecdanilova@gmail.com>
2022-11-11 11:47:48 -0500

    setup and style project

--- src/App.js
+++ src/App.js
@@ -25,8 +25,47 @@ function App() {
     <div className='block bcg-black'></div>
     <div className='block'>
       <div className='container'>
-        <img src={defaultImage} alt="Random user"
-        className='user-img'/>
+        <img src={(person && person.image) || defaultImage} /* if person is not known then look for person that image and if is not the case then display default image again */
+        alt="Random user" 
+        className='user-img'
+        />
+        <p className='user-title'>my {title} is</p>
+        <p className='user-value'>{value}</p>
+        <div className='values-list'>
+          <button className='icon'
+          data-label="name"
+          onMouseOver={handleValue}>
+            <FaUser/>
+          </button>
+          <button className='icon'
+          data-label="email"
+          onMouseOver={handleValue}>
+            <FaEnvelopeOpen/>
+          </button>
+          <button className='icon'
+          data-label="age"
+          onMouseOver={handleValue}>
+            <FaCalendarTimes/>
+          </button>
+          <button className='icon'
+          data-label="street"
+          onMouseOver={handleValue}>
+            <FaMap/>
+          </button>
+          <button className='icon'
+          data-label="phone"
+          onMouseOver={handleValue}>
+            <FaPhone/>
+          </button>
+          <button className='icon'
+          data-label="password"
+          onMouseOver={handleValue}>
+            <FaLock/>
+          </button>
+        </div>
+        <button className='btn' type='button'>
+          {loading ? 'loading...' : 'random user'}
+        </button>
       </div>
     </div>
   </main>
